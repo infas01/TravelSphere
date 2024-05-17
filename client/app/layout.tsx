ff8f713891c6ac37755a557e4ee71bacf63ec756
Infas_NM <87600043+infas01@users.noreply.github.com>
2024-05-18 00:33:19 +0530

    Installed Next JS

---
+++ client/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from 'next';
+import './globals.css';
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+
+export const metadata: Metadata = {
+  title: 'TravelSphere',
+  description: 'Enjoy with TravelSphere and Find a best place to visit',
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
